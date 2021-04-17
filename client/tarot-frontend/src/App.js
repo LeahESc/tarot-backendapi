2ec97c84341d529a8e1c45b112ae8da36294a6b8
LeahESc <leahemily1@gmail.com>
2021-04-17 10:26:42 -0700

    adds login methods and call to backend

--- client/tarot-frontend/src/App.js
+++ client/tarot-frontend/src/App.js
@@ -13,6 +13,32 @@ class App extends Component() {
       user: {}
     }
   }
+
+  handleLogin = (data) => { 
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', {withCredentials: true})
+    .then(resp => {
+      if (resp.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
   
   render() { 
 
