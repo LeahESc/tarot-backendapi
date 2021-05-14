b6bad59188541a3bf25e89ab2799bb877c216c9f
LeahESc <leahemily1@gmail.com>
2021-05-14 10:11:44 -0700

    migrates cards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_180342) do
+ActiveRecord::Schema.define(version: 2021_05_14_170913) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
